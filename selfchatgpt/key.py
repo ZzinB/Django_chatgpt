5e1ceed29556e068281a8b37fd0504c63489903f
Shinbi <mysteleee7@gmail.com>
2023-05-22 15:31:52 +0900

    update

---
+++ selfchatgpt/key.py
@@ -0,0 +1,7 @@
+import openai
+# Create your views here.
+
+openai.api_key = "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"
+
+def api_key(request):
+    return "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"
